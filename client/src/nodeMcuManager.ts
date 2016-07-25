b5d34f3d9ed2a32b9e043faf5b1d1cb3551b691b
Furkan DUMAN <furkan.duman@kaleyazilim.internal>
2016-07-25 11:50:33 +0300

    Extension need to run in Windows

--- client/src/nodeMcuManager.ts
+++ client/src/nodeMcuManager.ts
@@ -3,11 +3,13 @@ import { window } from "vscode";
 import * as path from "path";
 
 export function uploadFile(device: string, filename: string) {
-    let connector = new nodemcu.Connector(device, 9600);
+    // TODO: NodeMcu's default uart settings is 115200. Need to implement baudrate change function
+    let connector = new nodemcu.Connector(device, 115200);
 
     connector.connect((err, deviceInfo) => {
         if (err) {
             window.showErrorMessage("NodeMCU connection error: " + err);
+            connector.disconnect();
         }
         else {
             window.showInformationMessage("NodeMCU device connected: " + deviceInfo);
@@ -31,13 +33,24 @@ export function uploadFile(device: string, filename: string) {
 export function findDevice(cb: any): void {
     let connector = new nodemcu.Connector("", 9600);
 
-    connector.deviceInfo(false, (err, devices) => {
+    // serialport module cannot detect vendor id on windows so we should get all com devices and filtered by pnpid
+    let osIsWindows = process.platform == "win32";
+
+    connector.deviceInfo(osIsWindows, (err, devices) => {
         if (err) {
             window.showErrorMessage('Serial device connection error: ' + err);
         } else {
+            if (osIsWindows)
+            {
+                devices = devices.filter(function(device) {
+                    return device.pnpId.toString().includes("VID_1A86") || device.pnpId.toString().includes("VID_10C4");
+                });
+            }
+
             if (devices.length == 0) {
                 window.showErrorMessage('No Connected Devices found');
             } else if (devices.length > 1) {
+                // TODO: Display selection popup
                 window.showErrorMessage("Many devices found. Connect only once");
             }
             else {
