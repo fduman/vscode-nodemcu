ce36d18f91fc0bc79911d20be4c2099f776168cd
Furkan DUMAN <furkan.duman@kaleyazilim.internal>
2016-07-26 19:39:21 +0300

    parameter lookups in modules fixed

--- server/src/nodeMcuCompletionHandler.ts
+++ server/src/nodeMcuCompletionHandler.ts
@@ -7,7 +7,7 @@ import {
 import { modules } from "./completionItems/modules";
 import { lua } from "./completionItems/lua";
 import { api } from "./completionItems/api";
-import { analyze, AnalyzeResult } from "./documentAnalyzer" ;
+import { analyze, AnalyzeResult } from "./documentAnalyzer";
 
 export function nodeMcuCompletionHandler(remoteConsole: RemoteConsole, document: TextDocument, textDocumentPosition: TextDocumentPositionParams): CompletionItem[] {
     remoteConsole.log("Server nodeMcuCompletionHandler entered");
@@ -16,19 +16,14 @@ export function nodeMcuCompletionHandler(remoteConsole: RemoteConsole, document:
     let lineText = text.split(/\r?\n/g)[textDocumentPosition.position.line];
 
     let analyzeResult = analyze(lineText, textDocumentPosition.position);
-    
+
     remoteConsole.log("Server nodeMcuCompletionHandler analyze: " + JSON.stringify(analyzeResult));
-    
-    if (!analyzeResult.inSignatureDefinition) {
-        
-        if (analyzeResult.moduleName == null || !api[analyzeResult.moduleName])
-        {
-            return modules.concat(lua);
-        }
-        else if (api[analyzeResult.moduleName])
-        {
-            return api[analyzeResult.moduleName];
-        }
+
+    if (analyzeResult.moduleName == null || !api[analyzeResult.moduleName]) {
+        return modules.concat(lua);
+    }
+    else if (api[analyzeResult.moduleName]) {
+        return api[analyzeResult.moduleName];
     }
 
     return null;
